18104bb782341f4b65d9672d29d0b57aaf93c626
Sylvain Perrinel <sylvain.perrinel@agriculture.gouv.fr>
2018-04-10 09:45:34 +0200

    Test smtp with gomail

--- smtp.go
+++ smtp.go
@@ -1,51 +1,48 @@
 package emaildriver
 
 import (
+	"gopkg.in/gomail.v2"
 	"bytes"
 	"crypto/tls"
 	"errors"
-	"net"
-	"net/smtp"
 	"strconv"
+	"net"
 )
 
 type SmtpEmailBackend struct {
 	isEnabled  bool
-	smtpAuth   smtp.Auth
-	serverName string
-	conn       net.Conn
+	d gomail.Dialer
+	smtpAddress string
 }
 
 func (m *SmtpEmailBackend) Init(conf map[string]string) error {
-	var ok bool
 	var tlsConn bool
-	var login string
-	var password string
-	var port string
-	var err error
 
 	if conf == nil {
 		return errors.New("SmtpEmailBackend requires a configuration map")
 	}
 
-	m.serverName, ok = conf["servername"]
+
+	smtpUser, ok := conf["SMTPUsername"]
 	if !ok {
-		return errors.New("No servername defined, emails will fail")
+		return errors.New("SMTPPassword not defined")
 	}
 
-	login, ok = conf["login"]
+	smtpPass, ok := conf["SMTPPassword"]
 	if !ok {
-		return errors.New("No login defined, emails will fail")
+		return errors.New("SMTPPassword not defined, emails will fail")
 	}
 
-	password, ok = conf["password"]
-	if !ok {
-		return errors.New("No password defined, emails will fail")
+	host, portString, err := net.SplitHostPort(conf["SMTPAddress"])
+	if err != nil {
+		return err
 	}
 
-	port, ok = conf["port"]
-	if !ok {
-		port = "25"
+	m.smtpAddress = conf["SMTPAddress"]
+
+	port, err := strconv.Atoi(portString)
+	if err != nil {
+		return err
 	}
 
 	tlsConnTxt, ok := conf["tls"]
@@ -55,16 +52,9 @@ func (m *SmtpEmailBackend) Init(conf map[string]string) error {
 		tlsConn, _ = strconv.ParseBool(tlsConnTxt)
 	}
 
-	m.smtpAuth = smtp.PlainAuth("", login, password, m.serverName)
-
+	d := gomail.NewDialer(host, port, smtpUser, smtpPass)
 	if tlsConn {
-		tlsconfig := &tls.Config{
-			InsecureSkipVerify: true,
-			ServerName:         m.serverName,
-		}
-		m.conn, err = tls.Dial("tcp", m.serverName+":"+port, tlsconfig)
-	} else {
-		m.conn, err = tls.Dial("tcp", m.serverName+":"+port, nil)
+		d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
 	}
 
 	if err != nil {
@@ -79,12 +69,12 @@ func (m *SmtpEmailBackend) Init(conf map[string]string) error {
 func (m *SmtpEmailBackend) Send(emailMeta EmailMeta, emailData interface{}, textTemplateName TykTemplateName, htmlTemplateName TykTemplateName, OrgId string, Styles string) error {
 
 	if !m.isEnabled {
-		log.Warning("Plese check your email settings and restart Tyk Dashboard in order for notifications to work")
+		log.Warning("SMTP driver not initialized.")
 		return errors.New("Driver not initialised correctly")
 	}
 
 	// Generate strings from templates
-	var htmlDoc, txtDoc bytes.Buffer
+	var txtDoc bytes.Buffer
 
 	type superEmailData struct {
 		Data   interface{}
@@ -92,14 +82,7 @@ func (m *SmtpEmailBackend) Send(emailMeta EmailMeta, emailData interface{}, text
 	}
 
 	thisData := superEmailData{Data: emailData}
-	// Pull custom CSS
 	thisData.Styles = Styles
-
-	htmlErr := PortalEmailTemplatesHTML.ExecuteTemplate(&htmlDoc, string(htmlTemplateName), thisData)
-	if htmlErr != nil {
-		log.Error("HTML Template error: ", htmlErr)
-		return htmlErr
-	}
 
 	txtErr := PortalEmailTemplatesTXT.ExecuteTemplate(&txtDoc, string(textTemplateName), emailData)
 	if txtErr != nil {
@@ -107,55 +90,22 @@ func (m *SmtpEmailBackend) Send(emailMeta EmailMeta, emailData interface{}, text
 		return txtErr
 	}
 
-	// Do sending
-
 	fromStr := emailMeta.FromName + " <" + emailMeta.FromEmail + ">"
 	recStr := emailMeta.RecipientName + " <" + emailMeta.RecipientEmail + ">"
+	subj := emailMeta.Subject
 	msg := txtDoc.String()
 
-	c, err := smtp.NewClient(m.conn, m.serverName)
-	if err != nil {
-		log.Error("Unable to get SMTP Client :", err)
-		return err
+	message := gomail.NewMessage()
+	message.SetHeader("From", fromStr)
+	message.SetHeader("To", recStr)
+	message.SetHeader("Subject", subj)
+	message.SetBody("text/html", msg)
+
+	if err := m.d.DialAndSend(message); err != nil {
+		panic(err)
 	}
 
-	if err = c.Auth(m.smtpAuth); err != nil {
-		log.Error("Unable to authenticate with SMTP Client :", err)
-		return err
-	}
-
-	if err = c.Mail(fromStr); err != nil {
-		log.Error("Unable to set from for SMTP Client :", err)
-		return err
-	}
-
-	if err = c.Rcpt(recStr); err != nil {
-		log.Error("Unable to set recipient for SMTP Client :", err)
-		return err
-	}
-
-	// Data
-	w, err := c.Data()
-	if err != nil {
-		log.Error("Unable to set data for SMTP Client :", err)
-		return err
-	}
-
-	_, err = w.Write([]byte(msg))
-	if err != nil {
-		log.Error("Unable to write data for SMTP Client :", err)
-		return err
-	}
-
-	err = w.Close()
-	if err != nil {
-		log.Error("Unable to close data for SMTP Client :", err)
-		return err
-	}
-
-	c.Quit()
-
-	log.Info("Email sending (SMTP) ")
+	log.Debugf("email sent addr: %s, from: %s, to: %s", m.smtpAddress, fromStr, recStr)
 	if err != nil {
 		log.Error("SMTP error: ", err)
 		return err
