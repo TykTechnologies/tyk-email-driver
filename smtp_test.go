630a8ca94403e1dba7e6325dc2a74e40b51dd467
James Lucktaylor <jlucktay@users.noreply.github.com>
2020-03-05 12:06:04 +0000

    Update SMTP port handling (#19)
    - Migrate to Go modules.
      - The bug fix for SMTP address/port handling is required by another project, which has already been migrated to modules.
    - Refactor SMTP test with table pattern.
      - Make test cases discrete/isolated and add some description/clarity to each.
    - Refactor SMTP address and port parsing.

--- smtp_test.go
+++ smtp_test.go
@@ -1,33 +1,57 @@
 package emaildriver
 
-import "testing"
-
-var baseSMTPMap = map[string]string{}
+import (
+	"testing"
+)
 
 func TestSMTPEmailBackend_Init(t *testing.T) {
-
-	emailBackend := SMTPEmailBackend{}
-
-	if err := emailBackend.Init(nil); err == nil {
-		t.Error("expected driver initialization error with nil config")
+	testCases := map[string]struct {
+		conf        map[string]string
+		expectError bool
+	}{
+		"nil map`": {
+			conf:        nil,
+			expectError: true,
+		},
+		"empty map": {
+			conf:        map[string]string{},
+			expectError: true,
+		},
+		"junk address": {
+			conf: map[string]string{
+				"SMTPAddress": "junk",
+			},
+			expectError: true,
+		},
+		"valid just host": {
+			conf: map[string]string{
+				"SMTPAddress": "abc.com:123",
+			},
+			expectError: false,
+		},
+		"give port twice (ignore SMTPPort)": {
+			conf: map[string]string{
+				"SMTPAddress": "abc.com:123",
+				"SMTPPort":    "456",
+			},
+			expectError: false,
+		},
+		"valid with port": {
+			conf: map[string]string{
+				"SMTPAddress": "abc.com",
+				"SMTPPort":    "587",
+			},
+			expectError: false,
+		},
 	}
 
-	if err := emailBackend.Init(baseSMTPMap); err == nil {
-		t.Error("expected error with missing SMTPUsername")
-	}
-
-	baseSMTPMap["SMTPAddress"] = "junk"
-	if err := emailBackend.Init(baseSMTPMap); err == nil {
-		t.Error("expected error when unable to get host and port from SMTPAddress")
-	}
-
-	baseSMTPMap["SMTPAddress"] = "abc.com"
-	if err := emailBackend.Init(baseSMTPMap); err == nil {
-		t.Error("expected error with missing port in SMTPAddress")
-	}
-
-	baseSMTPMap["SMTPAddress"] = "abc.com:123"
-	if err := emailBackend.Init(baseSMTPMap); err != nil {
-		t.Error("valid config map Init should return nil")
+	for desc, tc := range testCases {
+		t.Run(desc, func(t *testing.T) {
+			emailBackend := SMTPEmailBackend{}
+			err := emailBackend.Init(tc.conf)
+			if (tc.expectError == true && err == nil) || (tc.expectError == false && err != nil) {
+				t.Fatalf("Did not get an error per expectation: %#v", err)
+			}
+		})
 	}
 }
