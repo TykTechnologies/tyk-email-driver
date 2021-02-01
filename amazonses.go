d053d7eb9ee7e7f69c8550e48ed7b7fa84692e93
Patricio Diaz <padiazg@gmail.com>
2021-02-01 13:12:20 -0300

    Solves TT-1015. Updated AWS-SES driver tu use official SDK

--- amazonses.go
+++ amazonses.go
@@ -3,16 +3,34 @@ package emaildriver
 import (
 	"bytes"
 	"errors"
-	"github.com/sourcegraph/go-ses"
+	"fmt"
+	"regexp"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ses"
 )
 
 type AmazonSESEmailBackend struct {
-	isEnabled        bool
-	endpoint         string
-	accessKeyId      string
-	secretAccessKey  string
-	config           ses.Config
+	isEnabled       bool
+	endpoint        string
+	accessKeyId     string
+	secretAccessKey string
+	sessionData     *session.Session
+	charSet         string
+	region          string
 }
+
+func getRegionFromEndpoint(e string) string {
+	r0 := regexp.MustCompile(`email(?:\-smtp)?\.(?P<region>[\w|\-]+?)\.amazonaws\.com`)
+	m0 := r0.FindAllStringSubmatch(e, -1)
+	if len(m0) > 0 {
+		return m0[0][1]
+	}
+	return ""
+} // getRegionFromEndpoint ...
 
 func (s *AmazonSESEmailBackend) Init(conf map[string]string) error {
 	var ok bool
@@ -21,9 +39,19 @@ func (s *AmazonSESEmailBackend) Init(conf map[string]string) error {
 		return errors.New("AmazonSESEmailBackend requires a configuration map")
 	}
 
-	s.endpoint, ok = conf["Endpoint"]
-	if !ok {
-		return errors.New("No Amazon SES endpoint defined, emails will fail")
+	// we can retrieve region from the Region parameter, or from the Endpoint parameter
+	s.region, _ = conf["Region"]
+	endPoint, _ := conf["Endpoint"]
+	if s.region == "" && endPoint == "" {
+		return errors.New("No Amazon SES region or endpoint defined, emails will fail")
+	}
+
+	// Endpoint provided, try to get region from there
+	if s.region == "" {
+		s.region = getRegionFromEndpoint(endPoint)
+		if s.region == "" {
+			return errors.New("Amazon SES region could not be retrieved from endpoint, emails will fail")
+		}
 	}
 
 	s.accessKeyId, ok = conf["AccessKeyId"]
@@ -36,17 +64,27 @@ func (s *AmazonSESEmailBackend) Init(conf map[string]string) error {
 		return errors.New("No Amazon SES secret access key defined, emails will fail")
 	}
 
-	s.config = ses.Config{
-	  Endpoint:        s.endpoint,
-	  AccessKeyID:     s.accessKeyId,
-	  SecretAccessKey: s.secretAccessKey,
+	s.charSet, ok = conf["CharSet"]
+	if !ok {
+		s.charSet = "UTF-8"
 	}
+
+	// Create a new session and specify an AWS Region.
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String(s.region),
+		Credentials: credentials.NewStaticCredentials(s.accessKeyId, s.secretAccessKey, ""),
+	})
+	if err != nil {
+		return errors.New("When creating Amazon SES session data")
+	}
+	s.sessionData = sess
+
 	s.isEnabled = true
 	return nil
-}
+} // Init ...
 
+// Send Sends emails using Amazon SES
 func (s *AmazonSESEmailBackend) Send(emailMeta EmailMeta, emailData interface{}, textTemplateName TykTemplateName, htmlTemplateName TykTemplateName, OrgId string, Styles string) error {
-
 	if !s.isEnabled {
 		log.Warning("Plese check your email settings and restart Tyk Dashboard in order for notifications to work")
 		return errors.New("Amazon SES email driver not initialised correctly")
@@ -78,14 +116,58 @@ func (s *AmazonSESEmailBackend) Send(emailMeta EmailMeta, emailData interface{},
 
 	from := "\"" + emailMeta.FromName + "\" <" + emailMeta.FromEmail + ">"
 	to := "\"" + emailMeta.RecipientName + "\" <" + emailMeta.RecipientEmail + ">"
-	response, err := s.config.SendEmailHTML(from, to, emailMeta.Subject, txtDoc.String(), htmlDoc.String())
 
-	log.Info("Email sending (AmazonSES)")
-	if err != nil {
-		log.Error("Amazon SES error: ", err)
-		log.Error("Response: ", response)
-		return err
+	// Assemble the email.
+	input := &ses.SendEmailInput{
+		Destination: &ses.Destination{
+			CcAddresses: []*string{},
+			ToAddresses: []*string{
+				aws.String(to),
+			},
+		},
+		Message: &ses.Message{
+			Body: &ses.Body{
+				Html: &ses.Content{
+					Charset: aws.String(s.charSet),
+					Data:    aws.String(htmlDoc.String()),
+				},
+				Text: &ses.Content{
+					Charset: aws.String(s.charSet),
+					Data:    aws.String(txtDoc.String()),
+				},
+			},
+			Subject: &ses.Content{
+				Charset: aws.String(s.charSet),
+				Data:    aws.String(emailMeta.Subject),
+			},
+		},
+		Source: aws.String(from),
 	}
 
+	// // Create an SES client in the session.
+	svc := ses.New(s.sessionData)
+
+	// Attempt to send the email.
+	_, err := svc.SendEmail(input)
+
+	if err != nil {
+		var errorText string
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case ses.ErrCodeMessageRejected:
+				errorText = fmt.Sprintf("%s: %v", ses.ErrCodeMessageRejected, aerr.Error())
+			case ses.ErrCodeMailFromDomainNotVerifiedException:
+				errorText = fmt.Sprintf("%s: %v", ses.ErrCodeMailFromDomainNotVerifiedException, aerr.Error())
+			case ses.ErrCodeConfigurationSetDoesNotExistException:
+				errorText = fmt.Sprintf("%s: %v", ses.ErrCodeConfigurationSetDoesNotExistException, aerr.Error())
+			default:
+				errorText = fmt.Sprintf("%v", aerr.Error())
+			}
+		} else {
+			errorText = fmt.Sprintf("%v", err.Error())
+		}
+		return errors.New(errorText)
+	} // if err != nil ...
+
 	return nil
-}
+} // Send
